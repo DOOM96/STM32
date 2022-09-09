85f58eb7727506809d027da6a9e5bc4b2b9de46e
Daniel <daosormo@hotmail.com>
2022-09-08 20:12:41 -0500

    Organize files in different directories (#1)

---
+++ SPI/src/declarations.h
@@ -0,0 +1,14 @@
+/*
+ * declarations.h
+ *
+ *  Created on: 03/05/2022
+ *      Author: daoso
+ */
+
+#ifndef DECLARATIONS_H_
+#define DECLARATIONS_H_
+
+extern char INPUT;
+
+
+#endif /* DECLARATIONS_H_ */
---
+++ uart_adc/src/declarations.h
@@ -0,0 +1,14 @@
+/*
+ * declarations.h
+ *
+ *  Created on: 01/04/2022
+ *      Author: daoso
+ */
+
+#ifndef DECLARATIONS_H_
+#define DECLARATIONS_H_
+
+extern uint16_t input;
+
+
+#endif /* DECLARATIONS_H_ */
